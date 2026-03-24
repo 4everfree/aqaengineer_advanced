398e9d5edfe6948c3356cb41bd9a891f5fe0c357
Vladislav Korobeinikov <vladkorobeinikov@gmail.com>
2026-03-24 22:50:23 +0400

    MR fix simple new email

--- tests/functional/put_v1_account_email/test_put_v1_account_email.py
+++ tests/functional/put_v1_account_email/test_put_v1_account_email.py
@@ -6,7 +6,7 @@ def test_put_v1_account_email(account_helper, create_user_data):
     account_helper.register_new_user(login=login, password=password, email=email)
     account_helper.user_login(login=login, password=password)
 
-    email = email.split('@')[0] + '1@' + email.split('@')[1]
+    email = 'tests@aol.com'
     account_helper.update_account_email(login=login, password=password, email=email)
 
     account_helper.user_login(login=login, password=password, status_code=403)
